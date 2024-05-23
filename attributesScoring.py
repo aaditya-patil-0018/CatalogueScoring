64486530711f8d49e2e9c01d827ed382b7db6628
aaditya-patil-0018 <ampatil.6204@gmail.com>
2024-05-24 01:10:00 +0530

    Added comments to the file

--- attributesScoring.py
+++ attributesScoring.py
@@ -46,7 +46,7 @@ class attributesScoring:
         for attribute in self.catalogueInformation:
             # converting the passed column data in the form of clean way
             cleanAttribute = re.sub(r'[^a-zA-Z0-9]', '', attribute).lower()
-            # addinf the attribute name to attributeList for marking that it has been traversed
+            # addin the attribute name to attributeList for marking that it has been traversed
             self.attributeList.append(cleanAttribute)
             # identifying and checking the attirbute is essential and needed column or not
             self.identifyAttribute(cleanAttribute)
@@ -85,44 +85,72 @@ class attributesScoring:
         return False
 
     def score(self):
-        total = 0
-        score = 0
+        '''
+            This function is used for scoring the attribute of the catalog.
+            We check all the attribute and score the cataog accordingly
+        '''
+        total = 0 # total number of attributes present
+        score = 0 # number of checked attributes
+        # this function will check all the attributes thorogly
         self.check()
+        # adding the scores
         for attribute in self.attributesDictionary:
             total += 1
             if self.attributesDictionary[attribute][0] == 1:
                 score += 1
+        # printing the score to the terminal
         print("")
         cprint(f"-> Score: {score}/{total}", "black", "on_light_cyan")
         print("\n----------------")
         cprint("Recommendations:", "light_yellow")
         print("----------------")
+        # calling the recommendation function
         recommendData = self.recommendation()
+        # returning all the data in dictionary format
         return {"score": score, "total": total, "percentage": score*10, "recommendation": recommendData}
 
     def recommendation(self):
+        '''
+            recommendation
+                This function is used to provide and suggest recommendations to the user.
+                Recommendations like: attributes to add, attributes to rename, attributes to remove.
+        '''
+
+        # data will store all the recommendation values
         data = {
             "add": [],
             "rename": [],
             "remove": []
         }
+
+        # this list stores all the attributes whose names are properly mentionend
         columnNamedProperly = []
+
+        # checking each attributes and its status
         for attribute in self.attributesDictionary:
+            
+            # if attribute is not present in the catalog then recommend to add it
             if self.attributesDictionary[attribute][0] == 0:
                 print(colored(f"Add: {attribute.capitalize()}", color="light_green"))
                 data["add"].append(attribute)
+            # else if attribute is present then add it to the columnNamedProperly list
             elif self.attributesDictionary[attribute][0] == 1:
                 columnNamedProperly.append(self.attributesDictionary[attribute][1])
-
+        
+        # looping through all the attribute list that are present in the catalog
         for attribute in self.attributeList:
+            # if that attribute is not present in the columnNamedProperly then we recommend to rename it, which is optional.
             if attribute not in columnNamedProperly:
                 print(colored(f"Rename: {attribute} [optional]", color="light_red"))
                 data["rename"].append(attribute)
+            # if attribute name is unname, then just recommend to remove that column
             elif "unname" in attribute.lower():
                 print(colored(f"Remove: {attribute}", color="light_red"))
                 data["remove"].append(attribute)
+        # returns the recommendation data
         return data
 
+# this function runs only when this file is runned
 if __name__ == "__main__":
     filename = input("Enter the Filename of Catalogue: ")
     '''
--- nullChecker.py
+++ nullChecker.py
@@ -16,27 +16,26 @@ class nullChecker:
     def __init__(self, CatalogueData, CatalogueInformation):
         self.catalogueData = CatalogueData
         self.catalogueInformation = CatalogueInformation
-        # self.nullData = {
-        #             "columns": {},
-        #             "totalNull": 0,
-        #             "totalPercent": 0
-        #         }
-        # self.checkNullValues()
 
     def checkNullValues(self):
-        # print(type(self.catalogueData))
-        columnsData = {}
-        totalNull = 0
-        totalPercent = 0
-        df = self.catalogueData
+        columnsData = {} # data to store column wise
+        totalNull = 0 # total number of null values
+        totalPercent = 0 # total precentage of null values
+        df = self.catalogueData 
         columns = df.columns
         valuesCount = 0
+        # checks every column data of the catalog
         for column in columns:
+            # this will give total number of null values in that column
             totalNullValues = int(df[column].isnull().sum())
+            # adding this number to total values count
             valuesCount += int(len(df[column]))
+            # storing this data under column data
             columnsData[column] = int(totalNullValues)
+            # incrementing total null values
             totalNull += int(totalNullValues)
             # totalPercent = float(round((( totalNull / valuesCount) * 100), 2))
+        # returning the values after the calculations
         print({"columns": columnsData, "totalNull": totalNull, "totalPercent": totalPercent})
         return {"columns": columnsData, "totalNull": totalNull, "totalPercent": totalPercent}
     
